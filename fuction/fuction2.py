55ec0ffdc0999db180e6926c08111af5b604ea59
Dhanush Kumar M <dhanushmadan404@gmail.com>
2025-10-09 19:52:11 +0530

    add the some file for skill improvement

---
+++ fuction/fuction2.py
@@ -0,0 +1,5 @@
+def name(n):
+    for i in n:
+       print(i)
+u=input("enter you name")
+name(u)
---
+++ fuction/function.py
@@ -0,0 +1,5 @@
+def number(u):
+    for i in range(1,u+1):
+        print(i)
+n=int(input("Enter your number"))
+number(n)
