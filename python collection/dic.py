a402d60655fafe80449cc2c6c980b2dc0dd19171
Dhanush Kumar M <dhanushmadan404@gmail.com>
2025-10-22 11:35:59 +0530

    task1

---
+++ python collection/dic.py
@@ -0,0 +1,10 @@
+userdata = {
+    "name":'Dhanush',
+    "age":23,
+    "mark":[12,23,12,22],
+    "makos":{
+        "name":'dhanush'
+    }
+}
+print(userdata)
+print(userdata["age"])
---
+++ python collection/main.py
@@ -0,0 +1,80 @@
+
+
+even_number = [2,4,6,8,10]
+print(even_number)
+print(even_number[4])
+print(len(even_number)-1)
+# it is collect the total number
+l=len(even_number)
+# it is var with index num minus 1 it'a find last digit
+print(even_number[l-1])
+
+
+# 14/10/25
+numbers = [10,20,30,40,50]
+
+# for i in range(0,len(numbers),+1):
+    # print(numbers[i])
+    
+# empty-list
+def add_numbers(n):
+    #start
+    #declare an empty list
+    result = []
+    for i in range(1,n+1,+1):
+        result.append(i)
+    print(result)
+    #stop
+    
+add_numbers(3)
+add_numbers(5)
+foo = [10,20,30,40,50,60,70,80,90,100]
+
+print(foo[1])
+print(foo[2])
+print(foo[-1])
+print(foo[-2])
+print(foo[-10])
+print(len(foo))
+(foo.append(110))
+(foo.append(120))
+print(foo)
+print(foo[11])
+
+##
+print(foo[3:7])
+print(foo[3:4])
+##
+print(foo[-7:-2])
+##
+print(foo[3:])
+print(foo[:7])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
